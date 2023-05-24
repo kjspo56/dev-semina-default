4d010681067a60179354cd5ea13839d39eef19e7
kjspo56 <kjspo56@gmail.com>
2023-05-25 00:06:41 +0900

    practice

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,18 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
 
         //DI를 사용하지 않을때, 다음과 같이 사용
 
-//        Dog dog = new Dog();
-//        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
 
-//        Owner owner1 = new Owner(dog);
-//        Owner owner2 = new Owner(cat);
+        Owner owner1 = new Owner(dog);
+        Owner owner2 = new Owner(cat);
 
-//        owner1.animalCry(); // 멍멍
-//        owner2.animalCry(); // 야옹
+        owner1.animalCry(); // 멍멍
+        owner2.animalCry(); // 야옹
 
     }
 
--- src/main/java/com/example/demo/demo.java
+++ src/main/java/com/example/demo/demo.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 interface Animal {
     String cry();
